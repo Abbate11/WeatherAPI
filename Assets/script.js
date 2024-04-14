75b44950aaf2b8f1225259568b3cb1d176b34de8
Christian Abbate <Christian@myabbate.com>
2024-04-14 12:27:55 -0400

    started js functions, completed form submit handler, and get weather funtions. Added some more CSS, get five day forecast and display weather functions still to do.

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,66 @@
 const apiKey = '1f764b67e18fc39697b1aad8bd0ac0de';
-const apiUrl = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+const citySearchEl = document.querySelector('#city-search');
+const iconContainerEl = document.querySelector('.middle-right')
+const cityNameContainerEl = document.querySelector('.name');
+const dateContainerEl = document.querySelector('.date');
+const statsContainerEl = document.querySelector('.stats');
+const fiveDayContainerEl = document.querySelector('.five-day');
+
+// Form Submit Handler
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+
+    const city = citySearchEl.ariaValueMax.trim();
+
+    if (city) {
+        getCityWeather(city);
+
+        citySearchEl.textContent = '';
+        cityNameContainerEl.textContent = '';
+        iconContainerEl.textContent = '';
+        dateContainerEl.textContent = '';
+        statsContainerEl.textContent = '';
+        fiveDayContainerEl.textContent = '';
+    } else {
+        alert('You have entered and invalid City name');
+    }
+};
+
+//get city weather (today)
+const getCityWeather = function (city) {
+    const apiUrl = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+    fetch(apiUrl) 
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayWeather(data, city);
+                });
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to fetch Weather');
+        });
+};
 
 
+// get 5 day forecast
+const getFiveDayForecast = function(city) {
+    const apiUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                
+            })
+        }
+    })
+}
+
+
+
+// display weather 
